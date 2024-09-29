7a0cca8c94ae712b6a263f3609739cefedb1a39c
theon <me@dschp.theoriun.com>
2024-09-30 01:56:39 +0900

    Set custom-file

--- emacs.el
+++ emacs.el
@@ -140,20 +140,6 @@
 (column-number-mode 1)
 (scroll-bar-mode 0)
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(menu-bar-mode nil)
- '(org-agenda-files '("~/org/main.org"))
- '(package-selected-packages
-   '(color-theme-sanityinc-solarized monokai-theme color-theme-sanityinc-tomorrow solarized-theme modus-themes ample-theme spacemacs-theme doom-themes dracula-theme magit))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Noto Sans Mono" :foundry "GOOG" :slant normal :weight regular :height 120 :width normal)))))
